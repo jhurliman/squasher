348a36c9979dfd2e013a8a27968fb4cb5bbdaa1b
John Hurliman <jhurliman@jhurliman.org>
2015-09-03 01:31:44 -0700

    A better squasher for a better world
    Squashed commit of the following:
    
    commit 5b17d436038296108cebde6e0f5fe6b6cce25506
    Author: John Hurliman <jhurliman@jhurliman.org>
    Date:   Thu Sep 3 01:31:26 2015 -0700
    
        Yep
    
    commit 933b07c85ec2668e2a63b3e7c6595ef20ef0debb
    Author: John Hurliman <jhurliman@jhurliman.org>
    Date:   Thu Sep 3 01:28:50 2015 -0700
    
        A better squasher.
    
        Squashed commit of the following:
    
        commit 881aa2502c30374c15a436515524857b8f0e92b4
        Author: John Hurliman <jhurliman@jhurliman.org>
        Date:   Thu Sep 3 01:27:52 2015 -0700
    
            More better
    
        commit 5e720df5d0613dbde48aed7fb5ce6b45e09595b3
        Author: John Hurliman <jhurliman@jhurliman.org>
        Date:   Thu Sep 3 01:01:41 2015 -0700
    
            Squashed commit of the following:
    
            commit 750dbb4a7e3de1ba0ceac4d043fc3c1e24bea97d
            Author: John Hurliman <jhurliman@jhurliman.org>
            Date:   Thu Sep 3 01:01:31 2015 -0700
    
                Trying another approach
    
            commit c855cd0264f6c9d54c4985c7dd878fcae281d142
            Author: John Hurliman <jhurliman@jhurliman.org>
            Date:   Thu Sep 3 00:49:32 2015 -0700
    
                Fix
    
            commit 6046b60a7c2d38dcebd8c0b02348e13577906161
            Author: John Hurliman <jhurliman@jhurliman.org>
            Date:   Thu Sep 3 00:48:12 2015 -0700
    
                Improvements
    
            commit b0e369440b5140dd84e658db596a5c103ae6188a
            Author: John Hurlman <jhurliman@jhurliman.org>
            Date:   Thu Sep 3 00:38:17 2015 -0700
    
                Tweak
    
            commit 829de22de9f4f5c87dc3429edca410756a49a6dc
            Author: John Hurlman <jhurliman@jhurliman.org>
            Date:   Thu Sep 3 00:36:53 2015 -0700
    
                First draft squasher.sh
    
            commit 0bcbb5a199c77918448a819306597a7abc85a648
            Merge: 9f2a4d8 62c984b
            Author: John Hurlman <jhurliman@jhurliman.org>
            Date:   Wed Sep 2 23:59:19 2015 -0700
    
                Fixed merge conflict
    
            commit 9f2a4d842ba5cebc6f85bcff26e3780bb7dfafb6
            Author: John Hurlman <jhurliman@jhurliman.org>
            Date:   Wed Sep 2 23:58:27 2015 -0700
    
                Updating squasher
    
            commit fb92e838584d20b7bb594f79940a0947d327f8f8
            Author: John Hurlman <jhurliman@jhurliman.org>
            Date:   Wed Sep 2 23:33:32 2015 -0700
    
                Added squasher.sh

--- squasher.sh
+++ squasher.sh
@@ -1,3 +1,67 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
+R="\x1B[1;31m"
+G="\x1B[1;32m"
+W="\x1B[0m"
 
+error () { echo -e "${R}$1${W}"; exit 1; }
+
+################################################################################
+
+# Sanity check
+GIT_DIR=$(git rev-parse --git-dir) || error "Cannot find a git directory in the current folder"
+
+# Confirm the current branch is the correct one
+CUR_BRANCH=$(git symbolic-ref --short HEAD) || error "Cannot get current branch.\
+ Are you in a detached head state?"
+if [[ "$CUR_BRANCH" -eq "master" ]]; then
+  error "Current branch is master. git checkout [your_feature_branch] and try again"
+fi
+
+# Make sure there are no unstaged changes
+if ! git diff-files --quiet --ignore-submodules --; then
+  git diff-files --name-status -r --ignore-submodules -- >&2
+  error "Aborting, you have unstaged changes"
+fi
+
+# Fetch and attempt to merge master into the current branch. Abort on conflict
+echo -e "${G}git pull origin master${W}"
+git pull origin master || error "Must be able to cleanly merge master. Aborting"
+
+# Get the common ancestor commit of our feature branch and master
+ANCESTOR=$(git merge-base "$CUR_BRANCH" master) || error "Failed to find branch\
+ point from master to current branch"
+
+# Get user confirmation
+echo -en "Squashing commits (rewriting history) in branch ${G}$CUR_BRANCH${W}. Proceed? [y/N] "
+read PROCEED
+if [[ $PROCEED != "y" && $PROCEED != "Y" ]]; then
+  exit 1
+fi
+
+# Rename this branch, create a new branch with the original name based on
+# current master, then merge squash the renamed feature branch in
+git branch -m "$CUR_BRANCH-temp" || error "Failed to create temp branch"
+git checkout master || { git branch -m "$CUR_BRANCH"; error "Failed to checkout master branch"; }
+git checkout -b "$CUR_BRANCH" || error "Failed to recreate feature branch"
+git merge --squash "$CUR_BRANCH-temp" || error "Failed to merge squash feature branch"
+
+# Prompt for a new commit message
+while [[ -z "$COMMIT_TITLE" ]]; do
+  echo
+  echo -en "${G}Enter the final commit title:${W} "
+  read COMMIT_TITLE
+done
+
+# Prepend the commit message
+SQUASH_MSG="$GIT_DIR/SQUASH_MSG"
+TMP_MSG=$(mktemp -t gitsquash) || error "mktemp failed"
+echo -e "$COMMIT_TITLE\n" | cat - "$SQUASH_MSG" > "$TMP_MSG" && mv "$TMP_MSG" "$SQUASH_MSG"
+
+git commit
+git branch -D "$CUR_BRANCH-temp"
+
+echo
+echo -e "${G}Success! Squashed branch $CUR_BRANCH"
+echo
+git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
